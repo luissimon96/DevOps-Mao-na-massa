c695c12dd181d1ba0f8d96c41f2b7b9646b28083
SimonUlbra <luissimon96@rede.ulbra.br>
2022-07-13 21:15:22 -0300

    correção de bugs

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -2,6 +2,7 @@
 sudo yum -y install epel-release
 echo "Início da instalação do Ansible"
 sudo yum -y install ansible
+ansible-galaxy install geerlingguy.mysql
 cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
@@ -12,3 +13,16 @@ cat <<EOT >> /etc/exports
 modprobe nfs
 modprobe nfsd
 EOT
+
+# Criando a relação de confiança nos hosts que o control-node acessa
+# Copiando a chave pública para os hosts remotos
+# Assim criamos a relação de confiança e o ssh na porta 22 vai ser liberado.
+# Caso não faça isso a máquina do control node não irá conseguir acessar as demais
+
+# no control-node gerar e depois buscar a chave publica e copiar
+# ssh-keygen # executar apenas uma vez para que seja possível a criação da chave pública e privada
+# cd /home/vagrant/.ssh
+# cat id_rsa.pub
+# copiar e colocar o conteúdo nos arquivos de privisionamento dos hosts
+
+# nas máquinas que o control-node irá acessar
